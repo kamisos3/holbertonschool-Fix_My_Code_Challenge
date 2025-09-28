c2db99a1afba5752e2b0e8f0c3232405fbbb4770
kamisos3 <kamilasostre11@gmail.com>
2025-09-28 19:40:27 -0400

    Function now updates the node and accepts the unknown data types

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,45 +11,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+	dlistint_t *tmp = *head, *to_delete;
+	unsigned int p = 0;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
+
+	if (index == 0)
 	{
+		to_delete = *head;
 		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(to_delete);
+		return (1);
+	}
+
+	while (tmp && p < index)
+	{
+		tmp = tmp->next;
 		p++;
 	}
-	if (p != index)
-	{
-		*head = saved_head;
+	if (!tmp)
 		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		tmp = *head;
-		if (tmp->prev)
-			tmp->prev->next = tmp->next;
-		if (tmp->next)
-			tmp->next->prev = tmp->prev;
-		free(tmp);
-		*head = saved_head;
-	}
+
+	if (tmp->prev)
+		tmp->prev->next = tmp->next;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+	free(tmp);
 	return (1);
 }
